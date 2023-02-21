aead4f6c6e55c11492ff0b96ed79700354598bdf
Eren <vrsozluk@gmail.com>
2023-02-22 01:22:09 +0300

    Testing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { TwitterEmbed } from 'react-social-media-embed';
+
 const Sidebar = () => {
   return (
     <div>
- <div style={{ display: 'flex', justifyContent: 'center' }}>
-  <TwitterEmbed url="https://twitter.com/ErenEroglu_Eng/status/1626699992713818116?s=20" width={325} />
-</div>
+
     </div>
   )
 }
