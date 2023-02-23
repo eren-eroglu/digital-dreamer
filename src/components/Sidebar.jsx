3bdb1d9b3f2107866ebf81c196f705a1fb540e69
Eren <vrsozluk@gmail.com>
2023-02-23 03:12:20 +0300

    Sidebar has been done and fully functioning

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,11 +6,16 @@ import {AiFillYoutube} from "react-icons/ai"
 import {AiFillMediumCircle} from "react-icons/ai"
 const Sidebar = () => {
   return <div className="flex flex-col space-y-10 mt-10 justify-end m-5">
-<AiFillTwitterCircle size={50} style={{ color: 'white' }}/>
-<AiFillLinkedin size={50} style={{ color: 'white' }}/>
-<AiFillGithub size={50} style={{ color: 'white' }}/>
-<AiFillYoutube size={50}  style={{ color: 'white' }}/>
-<AiFillMediumCircle size={50} style={{ color: 'white' }}/>
+    <a href="https://mobile.twitter.com/ErenEroglu_Eng">
+<AiFillTwitterCircle size={50} className="text-white transition-colors duration-300 hover:text-purple-500 hover:cursor-pointer"/></a>
+<a href="https://www.linkedin.com/in/eroglueren/">
+<AiFillLinkedin className="text-white transition-colors duration-300 hover:text-purple-500 hover:cursor-pointer" size={50} /></a>
+<a href="https://github.com/eren-eroglu">
+<AiFillGithub className="text-white transition-colors duration-300 hover:text-purple-500 hover:cursor-pointer" size={50} /></a>
+<a href="https://www.youtube.com/channel/UCotJX6uiRsp9zbqeP6KKTzQ">
+<AiFillYoutube className="text-white transition-colors duration-300 hover:text-purple-500 hover:cursor-pointer"  size={50}  /></a>
+<a href="https://medium.com/@ereneroglu">
+<AiFillMediumCircle className="text-white transition-colors duration-300 hover:text-purple-500 hover:cursor-pointer"  size={50} /></a>
   </div>;
 };
 
